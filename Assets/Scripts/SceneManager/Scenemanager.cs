c0d710ac2956c9dad9b137cbe1c9b34be97bea9a
Carlos <carloscreus@ceroca.cat>
2026-04-28 19:36:19 +0200

    Movimiento de la nave y delay funcional falta subirla al darle al boton

--- Assets/Scripts/SceneManager/Scenemanager.cs
+++ Assets/Scripts/SceneManager/Scenemanager.cs
@@ -1,19 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class SceneLoader : MonoBehaviour
 {
+    public float currentTimeLeft;
+    private bool isCountingDown;
+    private string pendingScene;
 
-
-    // Start is called before the first frame update
-
-//Este parametro hace que la escena siempre se escriba en cada boton en el onclick
-public void LoadScene(String SceneName)
+    void Awake()
     {
-        SceneManager.LoadScene(SceneName);
+        currentTimeLeft = 0f;
+        isCountingDown = false;
+    }
+
+    void Update()
+    {
+        if (!isCountingDown) return;
+
+        currentTimeLeft -= Time.deltaTime;
+        if (currentTimeLeft <= 0f)
+        {
+            isCountingDown = false;
+            SceneManager.LoadScene(pendingScene);
+        }
+    }
+
+    //Este parametro hace que la escena siempre se escriba en cada boton en el onclick
+    public void LoadScene(String SceneName, float delay)
+    {
+        pendingScene = SceneName;
+        currentTimeLeft = delay;
+        isCountingDown = true;
     }
 }

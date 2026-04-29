b834e722abdadd9386f3d61aaeaa29769471e5d2
Carlos <carloscreus@ceroca.cat>
2026-04-29 17:29:10 +0200

    Escena inici acabada

---
+++ Assets/Scripts/SceneManager/AnarAEscena.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnarAEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+public void cambiarEscenaAmbNom(String nomDeLaEscena)
+    {
+        SceneManager.LoadScene(nomDeLaEscena);
+    }
+}
